7d11593c1bcc8e28faf283a900b42585970e604e
Gianna <gianna.s.zhang@gmail.com>
2025-07-11 10:11:31 -0400

    Initial Commit

---
+++ BDay/BDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDayApp.swift
+//  BDay
+//
+//  Created by Gianna on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BDay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BDay
+//
+//  Created by Gianna on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
